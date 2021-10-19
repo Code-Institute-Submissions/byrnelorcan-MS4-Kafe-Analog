ca0a637d94f5e57f1e12b15d2ef52383eb61d234
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-19 22:09:51 +0000

    product models add

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Coffee, Category, Genre
+from .models import Coffee, Category, Genre, Vinyl, Condition
 
 # Register your models here.
 admin.site.register(Coffee)
 admin.site.register(Category)
 admin.site.register(Genre)
+admin.site.register(Vinyl)
+admin.site.register(Condition)
--- products/models.py
+++ products/models.py
@@ -14,17 +14,45 @@ class Category(models.Model):
 
 class Genre(models.Model):
     genre = models.CharField(max_length=254)
+  
     def __str__(self):
         return self.genre
 
 
+class Condition(models.Model):
+    condition = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.condition
+
+
 class Coffee(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    product_id = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    in_stock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vinyl(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    product_id = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    artist = models.CharField(max_length=254)
+    genre = models.ForeignKey('Genre', null=True, blank=True, on_delete=models.SET_NULL)
+    sub_genre = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    year = models.CharField(max_length=254)
+    condition = models.ForeignKey('Condition', null=True, blank=True, on_delete=models.SET_NULL)
     in_stock = models.BooleanField(default=True)
 
     def __str__(self):
