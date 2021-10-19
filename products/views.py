295306f471be8afa34395a98f444cd27a37bae5c
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-19 22:45:31 +0000

    initial database render

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Vinyl
 
 # Create your views here.
+
+
+def all_vinyl(request):
+    """ A view that renders all products page """
+
+    vinyls = Vinyl.objects.all()
+
+    context = {
+        'vinyls': vinyls,
+    }
+
+    return render(request, 'products/vinyls.html', context)
