46d1f4c1e8ed476f6681eb4090763d632e7ed5bb
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-19 22:19:46 +0000

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,36 @@ from django.contrib import admin
 from .models import Coffee, Category, Genre, Vinyl, Condition
 
 # Register your models here.
-admin.site.register(Coffee)
+
+
+class VinylAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'artist',
+        'category',
+        'price',
+        'image',
+        'in_stock',
+    )
+
+    ordering = ('product_id',)
+
+class CoffeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'category',
+        'price',
+        'image',
+        'in_stock',
+    )
+
+    ordering = ('product_id',)
+
+
+admin.site.register(Coffee, CoffeeAdmin)
 admin.site.register(Category)
 admin.site.register(Genre)
-admin.site.register(Vinyl)
+admin.site.register(Vinyl, VinylAdmin)
 admin.site.register(Condition)
