d05dbd968f2dc81e65f0c2302c1e60ed09b06037
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-21 11:59:27 +0000

    fix products page. All products show and will filter from nav

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_vinyl, name='vinyls')
+    path('', views.all_products, name='products')
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from .models import Vinyl
+from .models import Vinyl, Coffee
+
 
 # Create your views here.
-
-
-def all_vinyl(request):
+def all_products(request):
     """ A view that renders all products page """
 
     vinyls = Vinyl.objects.all()
+    coffees = Coffee.objects.all()
 
     context = {
         'vinyls': vinyls,
+        'coffees': coffees,
     }
 
-    return render(request, 'products/vinyls.html', context)
+    product_page = 'products/products.html'
+    return render(request, product_page, context)
+
