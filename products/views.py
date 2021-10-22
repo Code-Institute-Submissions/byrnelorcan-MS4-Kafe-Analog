37851b9ced54c6ea9d4a41d2b089d7b413c1d816
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-22 16:05:16 +0000

    product filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Products
+from .models import Products, Category
 
 
 # Create your views here.
@@ -10,19 +10,26 @@ def all_products(request):
 
     products = Products.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter a search?")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query) 
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
+        'current_categories': categories,
         'search_term': query,
     }
 
