36051b7bd0ed36716e87e3cc9807a66b9b5fe3ec
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-26 15:28:05 +0000

    add, edit and delete blogs

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Blog
+from .forms import BlogForm
 
 @login_required
 def all_blogs(request):
@@ -15,7 +17,8 @@ def all_blogs(request):
     blog = Blog.objects.all()
 
     context = {
-        'blog': blog
+        'blog': blog,
+        'blog_detail': blog_detail,
     }
 
     return render(request, 'blog/blog.html', context)
@@ -30,3 +33,70 @@ def blog_detail(request, blog_id):
     }
 
     return render(request, 'blog/blog_detail.html', context)
+
+
+@login_required
+def add_blog(request):
+    """ Add a blog to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog = form.save()
+            messages.success(request, 'Successfully added blog!')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to add blog. Please ensure the form is valid.')
+    else:
+        form = BlogForm()
+        
+    template = 'blog/add_blog.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_blog(request, blog_id):
+    """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated blog!')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to update blog. Please ensure the form is valid.')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {Blog.blog_title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
+
+@login_required
+def delete_blog(request, blog_id):
+    """ Delete a blog from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.delete()
+    messages.success(request, 'Blog deleted!')
+    return redirect(reverse('blog'))
