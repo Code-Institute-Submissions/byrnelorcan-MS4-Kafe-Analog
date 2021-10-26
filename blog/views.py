8155cceed255fac5b78884a3a5f9718bd1598bee
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-26 11:46:52 +0000

    blog url

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Blog
-from .forms import BlogForm
+
+def all_blogs(request):
+    """ View to show all blogs """
+
+    blog = Blog.objects.all()
+
+    context = {
+        'blog': blog
+    }
+
+    return render(request, 'blog/blog.html', context)
