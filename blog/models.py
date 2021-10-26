d5ba0bfff4d57c17d091438cbbafeed41cf44803
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-26 14:09:54 +0000

    club function

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Blog(models.Model):
     blog_title = models.CharField(max_length=254)
     blog_content = models.TextField()
+    blog_id = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,12 @@ from django.db.models.functions import Lower
 
 from .models import Blog
 
+@login_required
 def all_blogs(request):
     """ View to show all blogs """
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('login'))
 
     blog = Blog.objects.all()
 
@@ -15,3 +19,14 @@ def all_blogs(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+def blog_detail(request, blog_id):
+    """ A view for individual blog """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
