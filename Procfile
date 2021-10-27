e18606ba1cb4ac800022258399a28be67ce6db46
byrnelorcan <lorcan.byrne@icloud.com>
2021-10-27 01:52:50 +0000

    deployment attempt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kafe-analog.wsgi:application
--- kafe_analog/settings.py
+++ kafe_analog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ypr!1peou8otj$2aid+z%2wmqkrd)&8%875=z_tpjqauq*!ik!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kafe-analog.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +121,19 @@ WSGI_APPLICATION = 'kafe_analog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+        }
+     }
+
+
 
 
 # Password validation
