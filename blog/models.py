cc58b960bd34e3b54e879f15dc606a07cbfb07d0
byrnelorcan <lorcan.byrne@icloud.com>
2021-11-24 15:14:54 +0000

    fix for db issue

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,7 @@ from django.db import models
 
 class Blog(models.Model):
     blog_title = models.CharField(max_length=254, null=True, blank=True)
-    blog_author = models.CharField(max_length=254)
-    blog_source = models.CharField(max_length=254)
-    blog_paragraph_1 = models.TextField()
-    blog_paragraph_2 = models.TextField()
-    blog_paragraph_3 = models.TextField()
+    blog_content = models.TextField()
     blog_id = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
